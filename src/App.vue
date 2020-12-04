6ff237995672c066a63e749e9df8a9d6f1ab12cf
Maxim Vlasov <mxv.vlasov@gmail.com>
2020-12-04 10:31:38 +0300

    update

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,255 @@
 <template>
-  <div>
-    <h1>Stand</h1>
+  <h1>v1</h1>
+
+  <div class="grid">
+    <div class="wrap">
+      <div class="number">1</div>
+
+      <div class="video">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/S3Dpfyc15qQ"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </div>
+
+    <div class="wrap">
+      <div class="number">2</div>
+
+      <div class="video">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/S3Dpfyc15qQ?autoplay=1"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </div>
+
+    <div class="wrap">
+      <div class="number">3</div>
+
+      <div class="video">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/S3Dpfyc15qQ?loop=1&amp;playlist=S3Dpfyc15qQ&amp;autoplay=1&amp;"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </div>
+
+    <div class="wrap">
+      <div class="number">4</div>
+
+      <div class="video">
+        <iframe
+          frameborder="0"
+          allowfullscreen="1"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          title="YouTube video player"
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/S3Dpfyc15qQ?origin=http%3A%2F%2Flocalhost%3A8080&amp;enablejsapi=1&amp;autoplay=1&amp;autohide=1&amp;modestbranding=1&amp;disablekb=1&amp;controls=0&amp;playsinline=1&amp;loop=1&amp;widgetid=1"
+        ></iframe>
+      </div>
+    </div>
+
+    <div class="wrap">
+      <div class="number">5</div>
+
+      <div class="video">
+        <iframe
+          frameborder="0"
+          allowfullscreen="1"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          title="YouTube video player"
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/S3Dpfyc15qQ?origin=http%3A%2F%2Flocalhost%3A8080&amp;enablejsapi=1&amp;autoplay=1&amp;autohide=1&amp;modestbranding=1&amp;disablekb=1&amp;controls=0&amp;playsinline=1&amp;loop=1&amp;playlist=S3Dpfyc15qQ&amp;widgetid=5"
+          id="widget6"
+        ></iframe>
+      </div>
+    </div>
+
+    <div class="wrap">
+      <div class="number">6</div>
+
+      <div class="video">
+        <iframe
+          frameborder="0"
+          allowfullscreen="1"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          title="YouTube video player"
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/S3Dpfyc15qQ?origin=http%3A%2F%2Flocalhost%3A8080&amp;autoplay=1&amp;controls=0&amp;playsinline=1&amp;loop=1&amp;playlist=S3Dpfyc15qQ&amp;widgetid=5"
+          id="widget6"
+        ></iframe>
+      </div>
+    </div>
+
+    <div class="wrap">
+      <div class="number">7</div>
+
+      <div class="video">
+        <iframe
+          frameborder="0"
+          allowfullscreen="1"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          title="YouTube video player"
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/S3Dpfyc15qQ?origin=http%3A%2F%2Flocalhost%3A8080&amp;autoplay=1&amp;controls=0&amp;loop=1&amp;playlist=S3Dpfyc15qQ&amp;"
+          id="widget6"
+        ></iframe>
+      </div>
+    </div>
+  </div>
+
+  <h1>v2</h1>
+
+  <div class="grid">
+    <div class="wrap">
+      <div class="number">1</div>
+
+      <div class="video">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube-nocookie.com/embed/S3Dpfyc15qQ"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </div>
+
+    <div class="wrap">
+      <div class="number">2</div>
+
+      <div class="video">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube-nocookie.com/embed/S3Dpfyc15qQ?autoplay=1"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </div>
+
+    <div class="wrap">
+      <div class="number">3</div>
+
+      <div class="video">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube-nocookie.com/embed/S3Dpfyc15qQ?loop=1&amp;playlist=S3Dpfyc15qQ&amp;autoplay=1&amp;"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </div>
+
+    <div class="wrap">
+      <div class="number">4</div>
+
+      <div class="video">
+        <iframe
+          frameborder="0"
+          allowfullscreen="1"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          title="YouTube video player"
+          width="560"
+          height="315"
+          src="https://www.youtube-nocookie.com/embed/S3Dpfyc15qQ?origin=http%3A%2F%2Flocalhost%3A8080&amp;enablejsapi=1&amp;autoplay=1&amp;autohide=1&amp;modestbranding=1&amp;disablekb=1&amp;controls=0&amp;playsinline=1&amp;loop=1&amp;widgetid=1"
+        ></iframe>
+      </div>
+    </div>
+
+    <div class="wrap">
+      <div class="number">5</div>
+
+      <div class="video">
+        <iframe
+          frameborder="0"
+          allowfullscreen="1"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          title="YouTube video player"
+          width="560"
+          height="315"
+          src="https://www.youtube-nocookie.com/embed/S3Dpfyc15qQ?origin=http%3A%2F%2Flocalhost%3A8080&amp;enablejsapi=1&amp;autoplay=1&amp;autohide=1&amp;modestbranding=1&amp;disablekb=1&amp;controls=0&amp;playsinline=1&amp;loop=1&amp;playlist=S3Dpfyc15qQ&amp;widgetid=5"
+          id="widget6"
+        ></iframe>
+      </div>
+    </div>
+
+    <div class="wrap">
+      <div class="number">6</div>
+
+      <div class="video">
+        <iframe
+          frameborder="0"
+          allowfullscreen="1"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          title="YouTube video player"
+          width="560"
+          height="315"
+          src="https://www.youtube-nocookie.com/embed/S3Dpfyc15qQ?origin=http%3A%2F%2Flocalhost%3A8080&amp;autoplay=1&amp;controls=0&amp;playsinline=1&amp;loop=1&amp;playlist=S3Dpfyc15qQ&amp;widgetid=5"
+          id="widget6"
+        ></iframe>
+      </div>
+    </div>
+
+    <div class="wrap">
+      <div class="number">7</div>
+
+      <div class="video">
+        <iframe
+          frameborder="0"
+          allowfullscreen="1"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          title="YouTube video player"
+          width="560"
+          height="315"
+          src="https://www.youtube-nocookie.com/embed/S3Dpfyc15qQ?origin=http%3A%2F%2Flocalhost%3A8080&amp;autoplay=1&amp;controls=0&amp;loop=1&amp;playlist=S3Dpfyc15qQ&amp;"
+          id="widget6"
+        ></iframe>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
+
+<style scoped>
+.grid {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+}
+
+.number {
+  font-size: 32px;
+}
+</style>
